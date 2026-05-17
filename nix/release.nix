b7f510c7422ac851826733c804f02eb12da5b7c7
Joshua Kifer <joshua@moonlight.pm>
2026-05-17 10:14:02 -0600

    release: v0.1.0

--- nix/release.nix
+++ nix/release.nix
@@ -1,4 +1,4 @@
 {
-  version = "0.0.0-uninitialized";
-  hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  version = "0.1.0";
+  hash = "sha256-4qc2pTsnUyB3sp3D3bYEPXbNG3vhNrnk9iEDYll7pWE=";
 }
